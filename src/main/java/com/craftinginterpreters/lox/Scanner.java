3a0ae1a0cc23e9ba4c984b910a9ed78db6c1d2e3
bzhanggg <blz19@scarletmail.rutgers.edu>
2024-05-14 12:12:43 -0400

    finish Scanner class

--- src/main/java/com/craftinginterpreters/lox/Scanner.java
+++ src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -1,7 +1,9 @@
 package com.craftinginterpreters.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 // import static for convenience
 import static com.craftinginterpreters.lox.TokenType.*;
@@ -17,6 +19,27 @@ public class Scanner {
     private int current = 0;
     /** pointer to the current line of the source being scanned */
     private int line = 1;
+    /** map to check if an identifier lexeme is one of the reserved words */
+    private static final Map<String, TokenType> keywords;
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",    AND);
+        keywords.put("class",  CLASS);
+        keywords.put("else",   ELSE);
+        keywords.put("false",  FALSE);
+        keywords.put("for",    FOR);
+        keywords.put("fun",    FUN);
+        keywords.put("if",     IF);
+        keywords.put("nil",    NIL);
+        keywords.put("or",     OR);
+        keywords.put("print",  PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super",  SUPER);
+        keywords.put("this",   THIS);
+        keywords.put("true",   TRUE);
+        keywords.put("var",    VAR);
+        keywords.put("while",  WHILE);
+    }
 
     Scanner(String source) {
         this.source = source;
@@ -51,7 +74,7 @@ public class Scanner {
      * @param type the type of token the current lexeme is
      */
     private void addToken(TokenType type) {
-        addType(type, null);
+        addToken(type, null);
     }
 
     /**
@@ -59,7 +82,7 @@ public class Scanner {
      * @param type the type of the token of the current lexeme
      * @param literal the type of literal the lexeme represents, if it represents one
      */
-    private void addType(TokenType type, Object literal) {
+    private void addToken(TokenType type, Object literal) {
         String text = source.substring(start, current);
         tokens.add(new Token(type, text, literal, line));
     }
@@ -107,8 +130,19 @@ public class Scanner {
             case '\n':
                 line++;
                 break;
-            default:                                                  // case of lexical error
-                Lox.error(line, "Unexpected character.");
+            /* string literals */
+            case '"': string(); break;
+            default:
+                if (isDigit(c)) {
+                    /* number literals */
+                    number();
+                } else if (isAlpha(c)) {
+                    /* reserved words */
+                    identifier();
+                } else {
+                    /* lexical error */
+                    Lox.error(line, "Unexpected character.");
+                }
                 break;
         }
     }
@@ -133,5 +167,93 @@ public class Scanner {
     private char peek() {
         if (isAtEnd()) return '\0';
         return source.charAt(current);
+    }
+
+    /**
+     * Second character lookahead
+     * @return true, if there exists a character at `current+1`, otherwise the null terminator
+     */
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    /**
+     * Checks if the current character pointed to by current is a digit, for the case of a number lexeme
+     * @param c the character to check
+     * @return true iff c is a digit
+     */
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    /**
+     * Checks if the current character pointed to by current is an alphabet character (including the underscore char)
+     * @param c the current character pointed to by current
+     * @return true, iff c is alpha
+     */
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+    }
+
+    /**
+     * Checks if the current character pointed to by current is an alphanumeric character
+     * @param c the current character pointed to by current
+     * @return true, iff c is alphanumeric
+     */
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    /**
+     * Consumes characters until find the " that ends the string. Also handles case of unterminated string
+     */
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string.");
+            return;
+        }
+
+        // consume closing "
+        advance();
+
+        // trim surrounding quotes
+        String value = source.substring(start+1, current-1);
+        addToken(STRING, value);
+    }
+
+    /**
+     * Captures a number type, including decimals
+     */
+    private void number() {
+        while (isDigit(peek())) advance();
+
+        // Look for a fractional part
+        if (peek() == '.' && isDigit(peekNext())) {
+            // consume the '.'
+            advance();
+            while (isDigit(peek())) advance();
+        }
+
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    /**
+     * Captures a generic identifier, to be checked against the map of reserved words
+     */
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        // check to see if lexeme matches with a predefined reserved type
+        TokenType type = keywords.get(text);
+        // otherwise, the lexeme is a generic identifier
+        if (type == null) type = IDENTIFIER;
+        addToken(type);
     }
 }
